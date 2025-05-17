7fe268017b9c8a78d92d2aca8873f4051f23a4ee
Hao CHEN <hao.chen@cri-paris.org>
2025-05-17 14:25:47 +0200

    Add grpc client side logic to log body size

--- grpc_server/single_request_client.py
+++ grpc_server/single_request_client.py
@@ -57,8 +57,20 @@ def fetch_records(host: str, port: int, count: int, logger) -> None:
     # I.e., the time the received object is usable as an object with the client
     t_res = perf_counter_ns()
 
-    # 3. logging -----------------------------------------------------------
-    log_client(logger, t0=t0, t_req=t_req, t_res=t_res, req_id=req_id)
+    ## 4. Measure body size after query finish
+    req_size_bytes = len(request_pb.SerializeToString())
+    resp_size_bytes = len(response.SerializeToString())
+
+    # 5. logging -----------------------------------------------------------
+    log_client(
+        logger,
+        t0=t0,
+        t_req=t_req,
+        t_res=t_res,
+        req_id=req_id,
+        req_size_bytes=req_size_bytes,
+        resp_size_bytes=resp_size_bytes
+        )
     print('Finished')
 
 
